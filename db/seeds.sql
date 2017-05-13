4103b8817912a447c6ccc69124f14a1cd46fd5b9
Sergey60327 <sergeyshcherbakov@Employees-MacBook-Air-109.local>
2017-05-12 20:08:17 -0700

    New

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db3;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+
