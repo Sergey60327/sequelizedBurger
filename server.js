c850dedd0d9045baa9d2b9328c9dfa89314da1f8
Sergey60327 <sergeyshcherbakov@Employees-MacBook-Air-109.local>
2017-05-15 00:13:05 -0700

    New

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
+var exphbs = require('express-handlebars');
+var log = require("loglevel");
+var path = require("path");
 //process.env.PORT ||
 
 var port = process.env.PORT || 3000;
 var app = express();
+
+// Requiring our models for syncing
+var db = require(path.join(__dirname, '/models'));
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
